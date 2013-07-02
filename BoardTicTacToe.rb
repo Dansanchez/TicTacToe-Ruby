35ca4b224821be2b7d3535bda86036546912f8b1
Dansanchez <daniel.sanbre@gmail.com>
2013-07-02 13:10:29 -0600

    first commit

---
+++ BoardTicTacToe.rb
@@ -0,0 +1,61 @@
+class TicTacToeBoard
+
+ attr_accessor :board_matrix
+
+ def initialize
+   @board_matrix = [[BOARD_FLOOR,BOARD_FLOOR,BOARD_FLOOR],
+	 	    [BOARD_FLOOR,BOARD_FLOOR,BOARD_FLOOR],
+		    [BOARD_FLOOR,BOARD_FLOOR,BOARD_FLOOR]]
+ end
+
+
+ def make_move_player_1(row, column)
+   if @board_matrix[row][column]==
+   
+ def 
+
+
+
+#CONSTANTS
+TIC_TAC_TOE_BOARD_SIZE = 3
+BOARD_FLOOR = "|___|"
+MARK_PLAYER_1 = "|_X_|"
+MARK_PLAYER_2 = "|_O_|"
+ 
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
